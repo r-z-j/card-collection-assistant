37d0fe56ff670b4f225e2fe8b1fc10f38e4b8a47
Robert <Johnson>
2023-08-09 12:05:58 -0400

    sql script

--- capstone/java/database/data.sql
+++ capstone/java/database/data.sql
@@ -3,4 +3,26 @@ BEGIN TRANSACTION;
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
+INSERT INTO game_type (game_type_name, game_type_id) VALUES ('Magic', 1);
+INSERT INTO game_type (game_type_name, game_type_id) VALUES ('Pokemon', 2);
+
+INSERT INTO collection (collection_name, author_id, game_type_id) VALUES ('magic-collection-1', 1, 1);
+INSERT INTO collection (collection_name, author_id, game_type_id) VALUES ('poke-collection-1', 2, 2);
+
+INSERT INTO card (card_api_id, card_name, game_type_id) VALUES ('abcd1234', 'Black Lotus', 1);
+INSERT INTO card (card_api_id, card_name, game_type_id) VALUES ('hifjweio452987', 'Red Lotus', 1);
+INSERT INTO card (card_api_id, card_name, game_type_id) VALUES ('hjkgfd789754390', 'Cornflower Blue Lotus', 1);
+
+INSERT INTO card (card_api_id, card_name, game_type_id) VALUES ('vcbzxnmvzx', 'Charizard', 2);
+INSERT INTO card (card_api_id, card_name, game_type_id) VALUES ('89u06uifjkg', 'Gengar', 2);
+INSERT INTO card (card_api_id, card_name, game_type_id) VALUES ('63yui54o', 'Scyther', 2);
+
+INSERT INTO card_collection (card_id, collection_id) VALUES (1,1);
+INSERT INTO card_collection (card_id, collection_id) VALUES (2,1);
+INSERT INTO card_collection (card_id, collection_id) VALUES (3,1);
+
+INSERT INTO card_collection (card_id, collection_id) VALUES (4,2);
+INSERT INTO card_collection (card_id, collection_id) VALUES (5,2);
+INSERT INTO card_collection (card_id, collection_id) VALUES (6,2);
+
 COMMIT TRANSACTION;
--- capstone/java/database/schema.sql
+++ capstone/java/database/schema.sql
@@ -1,6 +1,11 @@
 BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS game_type;
+DROP TABLE IF EXISTS card;
+DROP TABLE IF EXISTS collection;
+DROP TABLE IF EXISTS card_collection;
+DROP TABLE IF EXISTS favorite_collection;
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -9,14 +14,42 @@ CREATE TABLE users (
 	role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id));
 
-DROP TABLE IF EXISTS collections;
+CREATE TABLE game_type (
+    game_type_id integer NOT NULL,
+    game_type_name varchar(50) NOT NULL,
+	CONSTRAINT PK_game_type PRIMARY KEY (game_type_id));
 
-CREATE TABLE collections (
+CREATE TABLE collection (
 	collection_id SERIAL,
-	collection_name varchar(50) NOT NULL UNIQUE,
-	author_id SERIAL,
-	game_type_id varchar(200) NOT NULL,
-    CONSTRAINT FK_author_id FOREIGN KEY(author_id) REFERENCES users(user_id),
+	collection_name varchar(100) NOT NULL UNIQUE,
+	author_id integer,
+	game_type_id integer NOT NULL,
+    CONSTRAINT FK_author_id FOREIGN KEY (author_id) REFERENCES users (user_id),
+    CONSTRAINT FK_game_type_id FOREIGN KEY (game_type_id) REFERENCES game_type (game_type_id),
 	CONSTRAINT PK_collection PRIMARY KEY (collection_id));
+
+CREATE TABLE card (
+    card_id SERIAL,
+    card_api_id varchar(200) NOT NULL,
+    card_name varchar(100) NOT NULL,
+	game_type_id integer NOT NULL,
+    CONSTRAINT FK_game_type_id FOREIGN KEY (game_type_id) REFERENCES game_type (game_type_id),
+	CONSTRAINT PK_card PRIMARY KEY (card_id));
+
+CREATE TABLE card_collection (
+    card_id integer NOT NULL,
+    collection_id integer NOT NULL,
+    CONSTRAINT PK_card_collection PRIMARY KEY (card_id, collection_id),
+    CONSTRAINT FK_card_collection_card FOREIGN KEY (card_id) REFERENCES card (card_id),
+    CONSTRAINT FK_card_collection_collection FOREIGN KEY (collection_id) REFERENCES collection (collection_id)
+    );
+
+CREATE TABLE favorite_collection (
+    user_id int NOT NULL,
+    collection_id integer NOT NULL,
+    CONSTRAINT PK_favorite_collection PRIMARY KEY (user_id, collection_id),
+    CONSTRAINT FK_favorite_collection_user FOREIGN KEY (user_id) REFERENCES users (user_id),
+    CONSTRAINT FK_favorite_collection_collection FOREIGN KEY(collection_id) REFERENCES collection (collection_id)
+    );
 
 COMMIT TRANSACTION;
