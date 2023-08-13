17e03e2687ef949bf8c12c8456578485fe98522a
Robert <Johnson>
2023-08-13 12:14:52 -0400

    Display MyCollectionsw

--- capstone/vue/src/views/MyCollections.vue
+++ capstone/vue/src/views/MyCollections.vue
@@ -2,6 +2,17 @@
   <div class="collections">
     <h1>Collections</h1>
     <button @click="getCollections">get collections</button>
+    
+    <div v-if="collections.length>0">
+
+        <div v-for="collection in collections" v-bind:key="collection.collectionId">
+            <div v-for="card in collection.cardList" v-bind:key="card.cardId">
+                <p>{{ card.cardName }} ${{ card.userPrice }}</p>
+            </div>
+        </div>
+
+    </div>
+
   </div>
 </template>
 
@@ -10,11 +21,17 @@ import collectionService from "../services/CollectionApiService";
 
 export default {
   name: "collections",
+  data() {
+      return {
+          collections: []
+      }
+  },
   methods: {
     async getCollections() {
       try {
         const res = await collectionService.getMyCollections()
           console.log(res.data);
+          this.collections = res.data
       } catch (e) {
         console.error("ERROR fetching collections:", e);
       }
