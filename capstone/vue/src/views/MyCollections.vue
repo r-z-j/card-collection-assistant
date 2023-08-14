39dad7c0bc79c80a9b200e74e1c36fc45f2ea78e
Amanda Duncan <alreindl21@gmail.com>
2023-08-14 11:48:49 -0400

    git fix

--- capstone/vue/src/views/MyCollections.vue
+++ capstone/vue/src/views/MyCollections.vue
@@ -1,27 +1,45 @@
 <template>
   <div class="collections">
     <h1>Collections</h1>
-     
-    
-
-        <div v-for="collection in myCollections" v-bind:key="collection.collectionId">
+        <div v-for="collection in collections " v-bind:key="collection.collectionId">
+          <h2>{{ collection.collectionName }}</h2>
             <div v-for="card in collection.cardList" v-bind:key="card.cardId">
                 <p>{{ card.cardName }} ${{ card.userPrice }}</p>
             </div>
         </div>
-
-
   </div>
 </template>
 
 <script>
+import collectionService from "../services/CollectionApiService"
 
 export default {
   name: "collections",
+  data() {
+    return {
+      collections: null,
+    }
+  },
+
   computed: {
     myCollections() {
       return this.$store.state.collections;
     }
   },
+
+  async created() {
+    const res = await this.getCollections();
+    this.collections = res.data;
+    // const res = await collectionService.getMyCollections();
+    // console.log(res.data);
+    // this.collections = res.data;
+  },
+
+  methods: {
+    getCollections: async() => {
+      return collectionService.getMyCollections();
+    }
+  },
+
 };
 </script>
